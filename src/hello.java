bdd9331157243d1b8125628e9fc416d14d656aa0
Nicherke <nicksoncheruiyot001@gmail.com>
2024-06-27 16:25:07 +0300

    java scanner and string

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String txt = scan.next();
+
+        System.out.println(txt);
+    }
+}
